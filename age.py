2ccc7cc5fb03e0a1de4ef3ad3ebee14a863a6d7d
Zero Requiem <60090339+zerorequiem20@users.noreply.github.com>
2024-03-12 14:52:44 +0000

    Create age.py

---
+++ age.py
@@ -0,0 +1,9 @@
+age = int(input("Enter your age: "))
+
+
+if age >= 18:
+    print('You are in category A')
+elif age >= 16 and age<18:
+    print('You are in category B')
+else:
+    print('You are in category C')
