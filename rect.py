2f6646061ed89ff447db75177c4fd93434b27090
Zero Requiem <60090339+zerorequiem20@users.noreply.github.com>
2024-03-12 14:18:50 +0000

    Create rect.py

---
+++ rect.py
@@ -0,0 +1,9 @@
+length = int(input("Enter the length of the rectangle"))
+width = int(input("Enter the width of the rectangle"))
+
+area = length*width
+perimeter = 2 * (length+width)
+
+print("The area of the rectangle is", area)
+print("The perimeter of the rectangle is", perimeter)
+
